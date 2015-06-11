0d90536b7da9a7bd59fefaf89171b0aa11d0e2cb
cho2MLHC <cho2.marsmellow@gmail.com>
2015-06-11 17:46:00 +0700

    dorong semua gan

---
+++ README.txt
@@ -0,0 +1,79 @@
+// Panduan Halaman
+
+o Error 404 : 404.html
+o Error 500 : 500.html
+o Under Construction : under-construction.html
+o Landing Page : index.html
+o Halaman 1 kolom : news-1-col.html
+o Halaman 2 kolom : news-2-col.html
+o Halaman 3 kolom : news-3-col.html
+o Halaman Berita : news-detail.html, news-1-col.html, news-2-col.html, news-3-col.html
+o Halaman Beranda : index.html
+o Halaman Gallery : gallery.html
+
+======================================================================================
+
+// Peta Situs
+
+o Beranda : index.html
+o Profil
+	o Sejarah : 404.html
+	o Visi dan Misi : 404.html
+	o Tugas dan Fungsi : 404.html
+	o Struktur Organisasi : 404.html
+	o Daftar Menteri : 404.html
+	o Unit Kerja : 404.html
+o Berita
+	o Kementerian : news-1-col.html
+	o Pemerintahan : news-2-col.html
+	o Sorotan Media : news-3-col.html
+	o Siaran Pers : under-construction.html
+o Layanan Publik
+	o Perizinan : 404.html
+	o Sertifikasi : 404.html
+	o Lelang (LPSE) : 404.html
+	o Pengaduan : 404.html
+	o Domain ID : 404.html
+	o e-Government : 404.html
+	o e-Business : 404.html
+o Informasi Publik
+	o Unduh : 404.html
+	o Statistik : 404.html
+	o Hasil Penelitian : 404.htmln
+	o Jurnal Ilmiah : 404.html
+	o Streaming : 404.html
+	o Sistem Informasi Geografis : 404.html
+	o Perjanjian/MOU : 404.html
+o Program
+	o Pemberdayaan Informatika : 404.html
+	o Telematika Pedesaan : 404.html
+	o Internet Sehat : 404.html
+	o Open Source : 404.html
+	o TV Digital : 404.html
+	o Monumen Pers Nasional : 404.html
+	o Pekan Informasi Nasional : 404.html
+	o Keamanan Informasi : 404.html
+	o Palapa Ring : 404.html
+	o Beasiswa : 404.html
+o Publikasi 
+	o Laporan Keuangan Tahunan : 404.html
+	o Laporan Akuntabilitas : 404.html
+	o Rencana Kerja Anggaran : 404.html
+	o Lakip : 404.html
+	o Renja : 404.html
+	o Renstra : 404.html
+o Lain-lain
+	o Agenda : 404.html
+	o Regulasi : 404.html
+	o Galeri : gallery.html
+	o Tautan : 404.html
+	o FAQ : 404.html
+
+Pencarian : semua halaman halaman
+Streaming Twitter : index.html, under-construction.html
+News Ticker : news-detail.html, news-1-col.html, news-2-col.html, news-3-col.html
+Popup Banner (modal) : news-detail.html tombol "Kirim"
+Slider Banner (carousel) : news-detail.html
+Footer : index.html
+RSS : footer index.html, news-detail.html, news-1-col.html, news-2-col.html, news-3-col.html
+Integrasi Media Sosial : news-detail.html 
---
+++ js/scripts.js
@@ -0,0 +1,123 @@
+$(document).ready(function() { 
+
+	"use strict";
+
+    /************** Slider Scripts **************/
+
+    $('.hero-slider').flexslider({
+        directionNav: false
+    });
+    $('.testimonials-slider').flexslider({
+        directionNav: false
+    });
+
+    $('.image-slider').flexslider({
+        animation: "slide",
+        directionNav: false
+    });
+
+    /************** Divider Scripts **************/
+
+    $('.background-image-holder').each(function() {
+
+        // Append background-image <img>'s as li item CSS background for better responsive performance
+        var imgSrc = $(this).children('.background-image').attr('src');
+        $(this).css('background', 'url("' + imgSrc + '")');
+        $(this).children('.background-image').hide();
+        $(this).css('background-position', '50% 0%');
+        // Check if the slider has a color scheme attached, if so, apply it to the slider nav
+    });
+
+}); 
+
+$(window).load(function() { 
+
+	"use strict";
+
+    var navHeight = $('nav').outerHeight();
+    $('.inner-link').smoothScroll({
+        offset: -navHeight,
+        speed: 800
+    });
+
+    /************** Parallax Scripts **************/
+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+    var isChrome = !!window.chrome;
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    var prefix;
+
+    if (isFirefox) {
+        prefix = '-moz-';
+    } else if (isIE) {
+
+    } else if (isChrome || isSafari) {
+        prefix = '-webkit-';
+    }
+
+    $('.parallax-background').each(function() {
+        $(this).attr('data-bottom-top', prefix + 'transform: translate3d(0px,-100px, 0px)');
+        $(this).attr('data-center', prefix + 'transform: translate3d(0px,0px, 0px)');
+        $(this).attr('data-top-bottom', prefix + 'transform: translate3d(0px,100px, 0px)');
+    });
+
+    if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+        skrollr.init({
+            forceHeight: false
+        });
+    }
+
+    $('.tweets-feed').flexslider({
+        directionNav: false,
+        controlNav: false
+    });
+
+
+    $('.instagram li a').attr('title', '');
+
+    setTimeout(function() {
+
+        $('.instagram li').each(function() {
+
+            // Append background-image <img>'s as li item CSS background for better responsive performance
+            var imgSrc = $(this).find('img').attr('src');
+            $(this).css('background', 'url("' + imgSrc + '")');
+            $(this).find('img').css('opacity', 0);
+            $(this).css('background-position', '50% 0%');
+            // Check if the slider has a color scheme attached, if so, apply it to the slider nav
+        });
+
+    }, 1000);
+
+    // Mailchimp/Campaign Monitor Mail List Form Scripts
+
+    $('form.mail-list-signup').on('submit', function() {
+
+        var iFrame = $(this).closest('section, header').find('iframe.mail-list-form'),
+
+            userEmail = $(this).find('.signup-email-field').val(),
+        userFullName = $(this).find('.signup-name-field').val(),
+        userFirstName = $(this).find('.signup-first-name-field').val(),
+        userLastName = $(this).find('.signup-last-name-field').val();
+
+        iFrame.contents().find('#mce-EMAIL, #fieldEmail').val(userEmail);
+        iFrame.contents().find('#mce-LNAME, #fieldLastName').val(userLastName);
+        iFrame.contents().find('#mce-FNAME, #fieldFirstName').val(userFirstName);
+        iFrame.contents().find('#mce-FNAME, #fieldName').val(userFullName);
+
+        iFrame.contents().find('form').attr('target', '_blank').submit();
+        return false;
+    });
+
+    setTimeout(function() {
+        $('.loader').addClass('hide-loader');
+        setTimeout(function() {
+            $('.loader').remove();
+            $('.main-container').addClass('show-content');
+            $('nav').addClass('show-content');
+        }, 500);
+    }, 10);
+
+
+}); 
